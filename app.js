f20e7c16b3862ba75537439dfc78e28ed09fde6a
aakshadubey <60819733+aakshadubey@users.noreply.github.com>
2022-09-19 13:29:59 +0530

    v-if, v-show, v-for, v-modal, lifecycle hooks, event handling

--- app.js
+++ app.js
@@ -1,6 +1,29 @@
+
 new Vue({
 	el: '#app',
 	data: {
-		show: true
-	}
+		show: true,
+        msg: "hello world",
+        count:0,
+        listOfFruit: ["grapes", "apple", "mango", "blueberry"] 
+	},
+    beforeCreate () {
+        console.log("beforeCreate")
+    },
+    created(){
+        console.log("created")
+    },
+    mounted (){
+        console.log("mounted")
+    },
+    updated (){
+        console.log("updated")
+    },
+    
+    methods: { 
+       increaseCounter(){
+          this.count +=1
+        }
+     }
+     
 })
